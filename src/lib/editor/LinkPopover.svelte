96ebf5e2793ebc000706bc7bdd73204f7a0cdd14
Ben Holmes <bholmesdev@gmail.com>
2025-07-27 09:12:49 -0400

    style link popover

--- src/lib/editor/LinkPopover.svelte
+++ src/lib/editor/LinkPopover.svelte
@@ -140,6 +140,7 @@
 		onfocusin={() => (status = 'focused')}
 		onfocusout={() => (status = 'idle')}
 	>
+		<div class="link-popover-inner">
 		<div class="input-container">
 			{#if status === 'idle'}
 				<span class="tab-label input-tab-label">Tab</span>
@@ -148,28 +149,58 @@
 		</div>
 		<button onclick={handleVisitLink}>
 			<span class="sr-only">Visit link</span>
-			{#if status === 'focused'}
-				<span class="tab-label">Tab</span>
-			{/if}
 			<Icon icon="mingcute:arrow-right-fill" />
 		</button>
+		</div>
 	</div>
 {/if}
 
 <style>
-	.link-popover {
-		position: absolute;
-		border: 1px solid #ccc;
-		border-radius: 4px;
-		width: calc(200px * var(--font-scale) * 0.75);
+	.link-popover-inner {
+		width: 100%;
+		background: #fff;
+		box-shadow:
+			0 -6px 3px 0 rgba(79, 95, 81, 0) inset,
+			0 -4px 3px 0 rgba(79, 95, 81, 0.01) inset,
+			0 -2px 2px 0 rgba(79, 95, 81, 0.05) inset,
+			0 -1px 2px 0 rgba(79, 95, 81, 0.09) inset,
+			0 0 1px 0 rgba(79, 95, 81, 0.1) inset;
+		border: none;
+		outline: none;
+		border-radius: 2px;
 		display: flex;
 		align-items: center;
+		gap: 0.2rem;
+	}
+
+	.link-popover {
+		position: absolute;
+		border-radius: 4px;
+		width: 300px;
+		display: flex;
 		font-size: var(--font-size-sm);
 		background-color: #fff;
 		transition:
-			translate 0.15s var(--cursor-transition-timing-function),
+			scale 0.15s var(--cursor-transition-timing-function),
 			left var(--cursor-transition-duration) var(--cursor-transition-timing-function),
 			top var(--cursor-transition-duration) var(--cursor-transition-timing-function);
+
+		border-radius: 2px;
+		background: rgba(39, 170, 58, 0.98);
+		box-shadow:
+			0 8px 2px 0 rgba(91, 123, 91, 0.01),
+			0 5px 2px 0 rgba(91, 123, 91, 0.04),
+			0 3px 2px 0 rgba(91, 123, 91, 0.14),
+			0 1px 1px 0 rgba(91, 123, 91, 0.24),
+			0 0 1px 0 rgba(91, 123, 91, 0.28);
+		padding-block-end: 2px;
+
+		translate: 0 -4px;
+		z-index: 1;
+	}
+
+	.link-popover:focus-within {
+		scale: 1.05;
 	}
 
 	.input-container {
@@ -178,25 +209,37 @@
 	}
 
 	button {
-		flex-shrink: 0;
+		height: 100%;
+		border: none;
+		border-radius: 0 2px 2px 0;
+		background: #28c840;
+		color: white;
+		font-size: var(--font-size-base);
+		box-shadow:
+			0 -6px 2px 0 rgba(39, 170, 58, 0.02) inset,
+			0 -4px 2px 0 rgba(39, 170, 58, 0.15) inset,
+			0 -2px 1px 0 rgba(39, 170, 58, 0.5) inset,
+			0 -1px 1px 0 rgba(39, 170, 58, 0.85) inset,
+			0 0 1px 0 rgba(39, 170, 58, 0.98) inset;
+		padding-inline: 0.6rem;
+		margin-block: -2px;
 		display: flex;
 		align-items: center;
 		justify-content: center;
-		gap: 4px;
-		line-height: 1;
 	}
 
 	input {
 		width: 100%;
 		height: 100%;
+		border: none;
+		outline: none;
+		padding: 0.4rem;
+		background-color: transparent;
 	}
 
 	.tab-label {
 		font-size: var(--font-size-xs);
-		font-weight: 500;
-	}
-
-	.input-tab-label {
+		font-family: var(--font-family-mono);
 		position: absolute;
 		top: 10%;
 		bottom: 10%;
@@ -208,9 +251,5 @@
 		display: flex;
 		align-items: center;
 		justify-content: center;
-	}
-
-	.link-popover:focus-within {
-		translate: 0 -4px;
 	}
 </style>
